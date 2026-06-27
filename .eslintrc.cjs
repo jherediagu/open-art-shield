7980af258ed23fc26d5c54778fc075c117f56652
Juan Heredia <jherediagu@gmail.com>
2026-06-27 23:51:07 +0200

    Set up pnpm monorepo (turbo, tsconfig, eslint/prettier)

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  env: {
+    node: true,
+    es2022: true,
+  },
+  ignorePatterns: ["dist/", "node_modules/", "*.config.ts", "*.config.js"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+    ],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+  },
+};
